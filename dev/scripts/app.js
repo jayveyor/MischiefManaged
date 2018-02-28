bea36d20696c4c450f04422b87b6c5cf13e16af2
Natalie Van Dine <nvandine@gmail.com>
2018-02-28 12:55:47 -0500

    Taco club break, selected for Hogwarts, destructured data and filtered Character Objects

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://www.potterapi.com/v1/characters', {
+      params: {
+        key: '$2a$10$uaBOxtF57MOEVumKnFQpeuQ.NDM5bS7lmAmb0gJHTWwarcz2HQam.',
+        school: "Hogwarts School of Witchcraft and Wizardry",
+      }
+    })
+    .then(({ data }) => {
+      data.forEach(function(characterObject){
+        console.log(characterObject);
+        return(characterObject);
+      });
+      // this.setState({
+      //   characters: this.i.house
+      // });
+    });
+  }
+
+
     render() {
       return (
         <div>
-          Hello
+          <p>{this.data}</p>
         </div>
       )
     }
