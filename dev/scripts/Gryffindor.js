87dfe086981b59c9a401c8df91b9ac1f0beed205
Meagan Moore <816moore@gmail.com>
2018-03-02 10:47:04 -0500

    trying to fix merge conflicts

--- dev/scripts/Gryffindor.js
+++ dev/scripts/Gryffindor.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import config from './config';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-// class Gryffindor extends React.Component {
-//     constructor () {
-//         super();
-//         this.state = {
+class Gryffindor extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            members: [],
+        }
 
-
-//         };
-//     }
-
-// }
-const Gryffindor = (props) => {
-    return ( 
-        <div>
-            {props.characterBio}
-        </div>
-    )
+    }
+    componentDidMount() {
+        axios.get(`https://www.potterapi.com/v1/houses/5a05e2b252f721a3cf2ea33f`, {
+            params: {
+                key: config.HPapiKey,
+            }
+        })
+        .then(({ data }) => {
+            // console.log(data[0].members);
+            this.setState({
+                members: data[0].members
+            });
+        });
+    }
+    
+    render(){
+        console.log(this.state.members);
+        return (
+            <div>
+                {this.state.members.map((member) => {
+                    return member.name
+                })}
+            </div>
+        )
+    }
 }
 
-// render() {
-//     return (
+// const Gryffindor = (props) => {
+//     return ( 
 //         <div>
-//             {props.gryffindor}
+//             {props.characterBio}
 //         </div>
 //     )
 // }
+
 export default Gryffindor;
