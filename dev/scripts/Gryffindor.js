ff7200c27e576ec65472110cdb61c4af5a4dcaee
Meagan Moore <816moore@gmail.com>
2018-03-05 13:05:11 -0500

    getting data for charts

--- dev/scripts/Gryffindor.js
+++ dev/scripts/Gryffindor.js
@@ -4,58 +4,84 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 // import Chart from './Chart';
       
-// const Gryffindor = (props) => {
-//     return (
-            
-//             <div>
-//         {props.characters.map((character) =>{
-//             // console.log(character)
-//         let gryffindor;
-//            let characterBio;
-//            characterBio = props.characterBio(character);
-//         if (character.house === "Gryffindor") {
-//             gryffindor = (characterBio)
-//             } 
-//             console.log(gryffindor)
 
-//             return (
-//                 <div>
-//                     {gryffindor}
-//                 </div>
-//             )
-//         })
-//     }
+
+let deathEaterCount = 0;
+let DACount = 0;
+let unaffiliatedCount = 0;
+const Gryffindor = (props) => {
+    return (
+            
+            <div>
+        {props.characters.map((character) =>{
+            // console.log(character)
+            let gryffindor;
+           let characterBio;
+           characterBio = props.characterBio(character);
+            if (character.house === "Gryffindor" && character.dumbledoresArmy === true) {
+                DACount = DACount + 1;
+                
+            } else if (character.house === "Gryffindor" && character.deathEater === true) {
+                deathEaterCount = deathEaterCount + 1;
+            } else {
+                unaffiliatedCount = unaffiliatedCount + 1;
+            }
+
+                gryffindor = (characterBio)
+                
+                // console.log(deathEaterCount);
+                console.log(DACount);
+                // console.log(unaffiliatedCount);
+                // console.log(character)
+
+            return (
+                <div>
+                    {gryffindor}
+                </div>
+            )
+        })
+    }
     
-//             </div>
-//         )
+            </div>
+        )
+}
+
+
+// if (character.dumbledoresArmy === true) {
+//     // console.log(`I am good`);
+//     DACount = DACount + 1;
+// } else if (character.deathEater === true) {
+//     // console.log(`I am evil`);
+//     deathEaterCount = deathEaterCount + 1;
+// } else {
+//     // console.log(`unaffiliated`)
+//     unaffiliatedCount = unaffiliatedCount + 1;
 // }
 
 
+// const Gryffindor = (props) => {
 
-
-const Gryffindor = (props) => {
-
-    const gryffinStudents = props.characters.map((character) => {
-        if (character.house === "Gryffindor") {
-            return character;
-        }
-    })
-    return (
-        <React.Fragment>
-            {gryffinStudents.map((character) => {
-                characterBio = props.characterBio(character);
-                console.log(character)
-                return (
-                    <div>
-                        {characterBio}
-                    </div>
-                )
-            } 
+//     const gryffinStudents = props.characters.map((character) => {
+//         if (character.house === "Gryffindor") {
+//             return character;
+//         }
+//     })
+//     return (
+//         <React.Fragment>
+//             {gryffinStudents.map((character) => {
+//                 characterBio = props.characterBio(character);
+//                 console.log(character)
+//                 return (
+//                     <div>
+//                         {characterBio}
+//                     </div>
+//                 )
+//             } 
                     
-        )}
+//         )}
 
-    </React.Fragment>
-    )};
+//     </React.Fragment>
+//     )};
 
 
 export default Gryffindor
