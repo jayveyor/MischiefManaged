0eb0e8d7022cd794597f3a365dcb86379287692a
Natalie Van Dine <nvandine@gmail.com>
2018-03-06 18:31:20 -0500

    pushing for pulling

--- dev/scripts/Gryffindor.js
+++ dev/scripts/Gryffindor.js
@@ -83,8 +83,8 @@ class Gryffindor extends React.Component {
                     params: {
                         format: 'json',
                         titles: char.name,
-                        width: 250,
-                        height: 250,
+                        width: 200,
+                        height: 200,
                     }
                 }
             });
